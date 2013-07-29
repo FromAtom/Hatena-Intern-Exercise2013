3856452e96ce15e5293dbea09b45c72eb3713db3
Atom <fromatom@gmail.com>
2013-07-30 02:23:58 +0900

    wrote js-3.js program and complete all basic exercise, may be.

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,12 @@
 // 課題 JS-3 の実装をここに記述してください。
+
+
+
+var submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', function (e) {
+    var textElement = document.getElementById("log-input");
+    var logArray = parseLTSVLog(textElement.value);
+    var containerElement = document.getElementById("table-container");
+
+    createLogTable(containerElement,logArray);
+}, false);
